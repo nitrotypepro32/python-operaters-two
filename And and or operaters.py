43bf22df0d6b499c1c68313f0939de57058ef8b5
nitrotypepro32 <pskreddy@gmail.com>
2025-03-07 19:41:13 -0600

    Add files via upload

---
+++ And and or operaters.py
@@ -0,0 +1,12 @@
+A=6
+B=5
+C=0
+if A and B and C: 
+   print ("All are greater than 0")
+else: 
+   print ("all are not greater than 0")
+if A or B or C: 
+   print ("Atleast one is greater than 0")
+else: 
+   print ("All are not greater than 0")
+
